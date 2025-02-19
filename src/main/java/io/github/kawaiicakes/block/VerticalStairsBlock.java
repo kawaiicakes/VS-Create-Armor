11f873b9611a1b952a8697b6c2a5fa718f419430
kawaiicakes <vomitmopper@gmail.com>
2025-02-18 17:58:18 -0700

    hopefully functioning VoxelShape generation for vertical stairs

--- src/main/java/io/github/kawaiicakes/block/VerticalStairsBlock.java
+++ src/main/java/io/github/kawaiicakes/block/VerticalStairsBlock.java
@@ -18,98 +18,12 @@ public class VerticalStairsBlock extends StairsBlock implements Waterloggable {
     public static final EnumProperty<VerticalStairShape> V_SHAPE
             = EnumProperty.of("shape", VerticalStairShape.class);
 
-    /*
-        // Superclass code work-through...
-
-        // { x∈N | x >= 0, x < 16 }
-
-        // (x & 1) != 0;
-        // true if x is odd
-
-        // (x & 2) != 0;
-        // true if x falls in the sequence 2, 3, 6, 7, 10, 11, 14, 15
-
-        // (x & 4) != 0;
-        // true if x falls in the sequence 4, 5, 6, 7, 12, 13, 14, 15
-
-        // (x & 8) != 0;
-        // true if x falls in the sequence 8, 9, 10, 11, 12, 13, 14, 15
-
-        // every single evaluation of x in range will be true for at least 8 values...
-        // each model part is either present or not based on the different evaluations of x
-        // of the 16 values of x, there are 8 values where a model part is present and 8 absent
-        // therefore, the code is returning the set of all possible combinations of model parts for the arg base passed
-
-        // (state.get(FACING)).getHorizontal();
-        // 0 when south, 1 when west, 2 when north, 3 when east
-
-        // (state.get(SHAPE)).ordinal() * 4
-        // 0 when straight, 4 when inner left, 8 when inner right, 12 when outer left, 16 when outer right
-
-        // (state.get(SHAPE)).ordinal() * 4 + (state.get(FACING)).getHorizontal();
-        // It appears that this effectively maps every possible combination of shapes & directions to 20 integers (0 to 19).
-        // This includes states that are different without visual distinction.
-        // Every increment of 4 starting from 0 "switches" the shape used.
-
-        // Each state's integer is matched to the index of its corresponding generated VoxelShape in SHAPE_INDICES.
-
-        // There are 12 visually unique shapes, but 20 blockstates. Each of the 20 blockstates possesses one of the 12.
-        // It should be clarified here that this is per block half. 12 unique shapes for 20 blockstates for top & bottom.
-
-        going back to #composeShape...
-
-        0 - base only (invalid)
-        1 - base + NW
-        2 - base + NE
-        3 - base + NW + NE
-        4 - base + SW
-        5 - base + NW + SW
-        6 - base + NE + SW (invalid)
-        7 - base + NW + NE + SW
-        8 - base + SE
-        9 - base + NW + SE (invalid)
-        10 - base + NE + SE
-        11 - base + NW + NE + SE
-        12 - base + SW + SE
-        13 - base + NW + SW + SE
-        14 - base + NE + SW + SE
-        15 - base + NW + NE + SW + SE (invalid)
-
-        for the vertical stairs the same premise exists, but is split between left & right rather than top & bottom.
-        Right and left are defined relative to the direction of the base and later expressed in absolute terms.
-        0 - base only (invalid)
-        1 - base + bottom right (inner bottom right)
-        2 - base + top right (inner top right)
-        3 - base + bottom right + top right (straight right)
-        4 - base + top left (inner top left)
-        5 - base + bottom right + top left (invalid)
-        6 - base + top right + top left (invalid)
-        7 - base + bottom right + top right + top left (outer top right)
-        8 - base + bottom left (inner bottom left)
-        9 - base + bottom right + bottom left (invalid)
-        10 - base + top right + bottom left (invalid)
-        11 - base + bottom right + top right + bottom left (outer bottom right)
-        12 - base + top left + bottom left (straight left)
-        13 - base + bottom right + top left + bottom left (outer bottom left)
-        14 - base + top right + top left + bottom left (outer top left)
-        15 - base + bottom right + top right + top left + bottom left (invalid)
-
-        TODO continue this
-
-        Shape indices for vertical stairs are now defined as the set of all combinations of shape with right/left.
-        0 - straight south
-        1 - straight west
-        2 - straight north
-        3 - straight east
-     */
-
-    // TODO
     public static final int[] RIGHT_INDICES = new int[] {
-            12, 5, 3, 10, 14, 13, 7, 11, 13, 7, 11, 14, 8, 4, 1, 2, 4, 1, 2, 8
+            3, 2, 1, 7, 11
     };
 
     public static final int[] LEFT_INDICES = new int[] {
-            12, 5, 3, 10, 14, 13, 7, 11, 13, 7, 11, 14, 8, 4, 1, 2, 4, 1, 2, 8
+            12, 4, 8, 14, 13
     };
 
     protected static final VoxelShape[] NORTH_SHAPES = composeShapes(
@@ -132,7 +46,7 @@ public class VerticalStairsBlock extends StairsBlock implements Waterloggable {
             BOTTOM_SOUTH_EAST_CORNER_SHAPE, TOP_SOUTH_EAST_CORNER_SHAPE
     );
 
-    public VerticalStairsBlock(BlockState baseBlockState, AbstractBlock.Settings settings) {
+    public VerticalStairsBlock(BlockState baseBlockState, Settings settings) {
         super(baseBlockState, settings);
         this.setDefaultState(
                 this.stateManager
@@ -146,13 +60,15 @@ public class VerticalStairsBlock extends StairsBlock implements Waterloggable {
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        // FIXME
-        return (state.get(HALF) == BlockHalf.RIGHT ? WEST_SHAPES : EAST_SHAPES)
-                [SHAPE_INDICES[this.getShapeIndexIndex(state)]];
-    }
+        int[] indices = state.get(HALF) == BlockHalf.RIGHT ? RIGHT_INDICES : LEFT_INDICES;
+        VoxelShape[] directionShapes = switch (state.get(FACING)) {
+            case NORTH -> NORTH_SHAPES;
+            case SOUTH -> SOUTH_SHAPES;
+            case WEST -> WEST_SHAPES;
+            default -> EAST_SHAPES;
+        };
 
-    private int getShapeIndexIndex(BlockState state) {
-        return state.get(V_SHAPE).ordinal() * 4 + state.get(FACING).getHorizontal();
+        return directionShapes[indices[state.get(V_SHAPE).ordinal()]];
     }
 
     @Override
